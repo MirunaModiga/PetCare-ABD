dccd6b07950bb9211013c545fe9ee7aa80b2cbf7
Modiga Miruna <107638226+MirunaModiga@users.noreply.github.com>
2023-12-07 10:18:03 +0200

    Add files via upload

---
+++ Src/MVVM/Model/ContactModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myPetCare.MVVM.Model
+{
+    class ContactModel
+    {
+        public string Username { get; set; }
+
+        public string ImageSource { get; set; }
+
+        public ObservableCollection<MessageModel> Messages { get; set; }
+
+        public string LastMessage => Messages.Any() ? Messages.Last().Message : "See messages";
+
+    }
+}
